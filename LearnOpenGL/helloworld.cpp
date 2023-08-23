85cf223387fea4cb6ce8424263d2e8d1a0566d8b
namunamunamu <kencoor9@gmail.com>
2023-08-23 21:24:59 +0900

    Shader 및 VAO, VBO, EBO 설정

--- LearnOpenGL/helloworld.cpp
+++ LearnOpenGL/helloworld.cpp
@@ -4,6 +4,35 @@
 
 using namespace std;
 
+float vertices[] = {
+	 0.5f, 0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	-0.5f, -0.5f, 0.0f,
+	-0.5f, 0.5f, 0.0f,
+};
+unsigned int indices[] = {
+	0, 1, 3,
+	1, 2, 3
+};
+
+int success;
+char infoLog[512];
+
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
+
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
@@ -22,6 +51,7 @@ int main() {
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 	//GLAD 초기화
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
@@ -29,9 +59,62 @@ int main() {
 		return -1;
 	}
 
-	//viewport 설정
-	glViewport(0, 0, 800, 600);
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	//VertexShader
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	//VertexShader compile log
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << endl;
+	}
+
+	//FragmentShader
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	//FragmentShader Compile Log
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		cout << "ERROR::SHADER::FRAMGENT::COMILATION_FAILED\n" << infoLog << endl;
+	}
+
+	//ShaderProgram
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	//ShaderProgram Log
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		cout << "ERROR::SHADER::PROGRAM::COMILATION_FAILED\n" << infoLog << endl;
+	}
+	//불필요한 리소스 삭제
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	//Vertex Buffer Object(VBO), Vertex Array Object 설정
+	unsigned int VAO, VBO, EBO; //Vertex Buffer Object, Vertex Array Object, Element Buffer Object
+	glGenVertexArrays(1, &VAO); //VAO 어레이 생성
+	glGenBuffers(1, &VBO); //VBO 버퍼 생성
+	glGenBuffers(1, &EBO); //EBO 버퍼 생성
+	glBindVertexArray(VAO); //VAO 바인딩
+	glBindBuffer(GL_ARRAY_BUFFER, VBO); //GL_ARRAY_BUFFER로 VBO 바인딩
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO); //GL_ELEMENT_ARRAY_BUFFER로 EBO 바인딩
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); //GL_ARRAY_BUFFER에 vertices 데이터 복사
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); //GL_ELEMENT_ARRAY_BUFFER에 indices 데이터 복사
+
+	//정점 속성 연결
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
 	//render loop
 	while (!glfwWindowShouldClose(window)) {
@@ -39,10 +122,19 @@ int main() {
 
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+		
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+	//리소스 삭제
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
 
 	glfwTerminate();
 	return 0;
